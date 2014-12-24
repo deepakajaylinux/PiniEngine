694f5344adef1bc549ffb1efd947f490a2b678a4
윤한석 <dnjstmxhs@naver.com>
2014-12-24 17:14:20 +0900

    수정

---
+++ docs/_build/html/_static/copyClipboard.js
@@ -0,0 +1,13 @@
+var client = new ZeroClipboard( document.getElementById("copy-button") );
+
+client.on( "ready", function( readyEvent )
+{
+  // alert( "ZeroClipboard SWF is ready!" );
+  client.on( "aftercopy", function( event )
+  {
+    // `this` === `client`
+    // `event.target` === the element that was clicked
+    event.target.style.display = "none";
+    alert("다음과 같은 내용이 복사 되었습니다. : " + event.data["text/plain"] );
+  } );
+} );
