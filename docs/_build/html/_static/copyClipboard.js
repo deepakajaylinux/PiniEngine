14fb2c50d64dded4e57ca5f104331e0fc7aa5b35
윤한석 <dnjstmxhs@naver.com>
2014-12-26 11:32:21 +0900

    수정

--- docs/_build/html/_static/copyClipboard.js
+++ docs/_build/html/_static/copyClipboard.js
@@ -7,7 +7,7 @@ client.on( "ready", function( readyEvent )
   {
     // `this` === `client`
     // `event.target` === the element that was clicked
-    event.target.style.display = "none";
+    // event.target.style.display = "none";
     alert("다음과 같은 내용이 복사 되었습니다. : " + event.data["text/plain"] );
   } );
 } );
